9f2dd0f0788725e21d57b185f66ab745c3e1f563
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-23 00:56:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,12 +2,15 @@ def current_player (board)
 
 end
 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+
 def turn_count (board)
   counter = 0
-  index = 0
+  # index = 0
   board.each do
+    index = 0
     if board[index] == "X" || board[index] == "0"
-      
+
       index += 1
       counter += 1
     end
