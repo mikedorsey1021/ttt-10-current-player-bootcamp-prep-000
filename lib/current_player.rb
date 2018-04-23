9825e455b6877136c4e6453ea6860d50125b826c
mikedorsey1021@gmail.com dorsey <mikedorsey1021@gmail.com>
2018-04-23 00:34:06 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def current_player (board)
+
+end
+
+def turn_count (board)
+  counter = 0
+
+  board.each do |x|
+    if board[x] == "X" || board[x] == "0"
+      counter += 1
+    end
+  end
+end
